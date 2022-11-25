6e91386030c528412e614b9070bda0a5d2857c65
PeterHsiao <peter12218@gmail.com>
2022-11-25 17:29:44 +0800

    styles

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,7 @@ import Search from "../components/Search"
 const Homepage = () => {
     const auth = "563492ad6f91700001000001bb3fccf57572421e8bdd03bc8241e39a"
     return (
-        <div>
+        <div style={{minHeight: "100vh"}}>
             <Search/>
         </div>
     )

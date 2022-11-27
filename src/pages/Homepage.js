f9344bb80dbe7d4e2792e9efd49c40aaa7469a9f
PeterHsiao <peter12218@gmail.com>
2022-11-27 23:04:04 +0800

    not complete

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,14 +4,19 @@ import Picture from '../components/Picture';
 
 const Homepage = () => {
     let [input, setInput] = useState("");
-    const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+    let [page, setPage] = useState(2); 
+    // page initial value should be "2" because we load Homepage initially is using "initialURL". It's parameter page value is "1".
+    // But if we need to load more picture, we must load pictures begin with page "2".
+    let [currentSearch, setCurrentSearch] = useState("");
+    const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
     const auth = "563492ad6f91700001000001bb3fccf57572421e8bdd03bc8241e39a";
     const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
     let [data, setData] = useState(null);
 
     // fetch data from pexels api
-    const search = async(searchURL) => {
-        const dataFetch = await fetch(searchURL, {
+    const search = async(url) => {
+        setPage(2);
+        const dataFetch = await fetch(url, {
             method: "GET", 
             headers: {
                 Accept: "application/json",
@@ -23,13 +28,42 @@ const Homepage = () => {
         // console.log(parseData);
     }
 
+    // load more picture
+    const morepicture = async() => {
+        let newURL;
+        if(input === "") {
+            newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+        } else {
+            newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+        }
+        setPage(page+1);
+        const dataFetch = await fetch(newURL, {
+            method: "GET", 
+            headers: {
+                Accept: "application/json",
+                Authorization: auth
+            }
+        });
+        let parseData = await dataFetch.json();
+        setData(data.concat(parseData.photos)); // array1.concat(array2) -> conbines array1 with array2
+    }
+
     // fetch data when the page loads up
     useEffect(() => {
         search(initialURL);
     }, []);
+    useEffect(() => {
+        if(currentSearch == "") {
+            search(initialURL);
+        } else {
+            search(initialURL);
+        }
+        search(currentSearch);
+    }, [currentSearch]);
+
     return (
         <div style={{minHeight: "100vh"}}>
-            <Search search={() => {search(searchURL)}} setInput={setInput} input={input}/>
+            <Search search={() => {search(searchURL); setCurrentSearch(input);}} setInput={setInput} input={input}/>
             <div className="pictures">
                 {data && data.map((d) => { 
                     // data will be null in the initial. Hence, "map" function will been error.
@@ -39,7 +73,7 @@ const Homepage = () => {
                 })}
             </div>
             <div className="morePicture">
-                <button>Load More</button>
+                <button onClick={morepicture}>Load More</button>
             </div>
         </div>
     )
